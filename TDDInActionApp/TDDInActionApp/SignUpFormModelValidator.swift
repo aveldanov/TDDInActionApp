9c76b51a7dd968e7236bca00bf311caf9620da82
Anton Veldanov <anton.veldanov@gmail.com>
2021-02-07 17:41:04 -0800

    iOS

--- TDDInActionApp/TDDInActionApp/SignUpFormModelValidator.swift
+++ TDDInActionApp/TDDInActionApp/SignUpFormModelValidator.swift
@@ -35,4 +35,20 @@ class SignUpFormModelValidator{
     }
     
     
+    func isPasswordValid(password: String) -> Bool {
+        var returnValue = true
+        
+        if password.count < SignupConstants.passwordMinLength ||
+        password.count > SignupConstants.passwordMaxLength {
+            returnValue = false
+        }
+        
+        return returnValue
+    }
+    
+    func doPasswordsMatch(password: String, repeatPassword: String) -> Bool {
+        return password == repeatPassword
+    }
+    
+    
 }
