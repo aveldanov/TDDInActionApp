40f9093f467488b27ff2d774e2b7a0ea00128225
Anton Veldanov <anton.veldanov@gmail.com>
2021-02-07 17:40:36 -0800

    iOS

--- TDDInActionApp/TDDInActionAppTests/SignUpFormModelValidatorTests.swift
+++ TDDInActionApp/TDDInActionAppTests/SignUpFormModelValidatorTests.swift
@@ -102,5 +102,53 @@ class SignUpFormModelValidatorTests: XCTestCase {
     }
     
     
+    
+    
+    // MARK: Password Validation
+    func testSignupFormModelValidator_WhenValidPasswordProvided_ShouldReturnTrue() {
+        
+        // Act
+        let isPasswordValid = sut.isPasswordValid(password: "12345678")
+        
+        // Assert
+        XCTAssertTrue(isPasswordValid, "The isPasswordValid() should have returned TRUE for a valid password  but it has returned FALSE")
+        
+    }
+    
+    func testSignupFormModelValidator_WhenTooShortPasswordProvided_ShouldReturnFalse() {
+        
+        // Act
+        let isPasswordValid = sut.isPasswordValid(password: "12")
+        
+        // Assert
+        XCTAssertFalse(isPasswordValid, "The isPasswordValid() should have returned FALSE for a password that is shorter than \(SignupConstants.passwordMinLength) but it has returned TRUE")
+        
+    }
+    
+    func testSignupFormModelValidator_WhenTooLongPasswordProvided_ShouldReturnFalse() {
+        
+        // Act
+        let isPasswordValid = sut.isPasswordValid(password: "12345678901234567")
+        
+        // Assert
+        XCTAssertFalse(isPasswordValid, "The isPasswordValid() should have returned FALSE for a password that is longer than \(SignupConstants.passwordMaxLength) but it has returned TRUE")
+        
+    }
+    
+    func testSignupFormModelValidator_WhenEqualPasswordsProvided_ShouldReturnTrue() {
+        // Act
+        let doPasswordsMatch = sut.doPasswordsMatch(password: "12345678", repeatPassword:"12345678")
+        
+        // Assert
+        XCTAssertTrue(doPasswordsMatch, "The doPasswordsMatch() should have returned TRUE for matching passwords but it has returned FALSE")
+    }
+    
+    func testSignupFormModelValidator_WhenNotMatchingPasswordsProvided_ShouldReturnFalse() {
+        // Act
+        let doPasswordsMatch = sut.doPasswordsMatch(password: "12345678", repeatPassword: "1234678")
+        
+        // Assert
+        XCTAssertFalse(doPasswordsMatch, "The doPasswordsMatch() should have returned FALSE for passwords that do not match but it has returned TRUE")
+    }
 
 }
