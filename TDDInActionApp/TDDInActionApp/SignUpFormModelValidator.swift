0ad5e4faa2a00468d091b3899347ca4dbcc0b227
Anton Veldanov <anton_veldanov@intuit.com>
2021-03-22 18:25:45 -0700

    iOS

--- TDDInActionApp/TDDInActionApp/SignUpFormModelValidator.swift
+++ TDDInActionApp/TDDInActionApp/SignUpFormModelValidator.swift
@@ -26,11 +26,9 @@ class SignUpFormModelValidator{
         return returnValue
     }
     
-    
     func isValidEmailFormat(email: String) -> Bool {
        return NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}").evaluate(with: email)
     }
-    
     
     func isPasswordValid(password: String) -> Bool {
         var returnValue = true
