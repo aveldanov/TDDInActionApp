8777f081adaa4e401fbf5e0b1ec9536a4d013a61
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-20 19:51:11 -0700

    iOS

---
+++ TDDPhtotoAppFeatures/TDDPhtotoAppFeatures/SignUpFormModelValidator.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpFormModelValidator.swift
+//  TDDPhtotoAppFeatures
+//
+//  Created by Anton Veldanov on 6/20/21.
+//
+
+import Foundation
+
+ //MARK: Validates Form input
+class SignUpFormModelValidator {
+    
+}
--- TDDPhtotoAppFeatures/TDDPhtotoAppFeaturesTests/SignUpFormModelValidatorTests.swift
+++ TDDPhtotoAppFeatures/TDDPhtotoAppFeaturesTests/SignUpFormModelValidatorTests.swift
@@ -7,14 +7,31 @@
 
 import XCTest
 
+@testable import TDDPhtotoAppFeatures
+
 class SignUpFormModelValidatorTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+    }
+    
+    
+    func testSignUpModelValidator_WhenValidFirstNameProvided_ShouldReturnTrue(){
+        
+        //Arrange
+        let sut = SignInFormModelValidator()
+        
+        
+        //Act
+        
+        
+        //Assert
+        
+        
     }
 
 
