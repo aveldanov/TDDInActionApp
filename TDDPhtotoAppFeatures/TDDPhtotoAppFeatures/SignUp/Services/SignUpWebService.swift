24f306934680da5816367d3b80d0e9d4f03422e0
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-20 21:15:03 -0700

    iOS

--- TDDPhtotoAppFeatures/TDDPhtotoAppFeatures/SignUp/Services/SignUpWebService.swift
+++ TDDPhtotoAppFeatures/TDDPhtotoAppFeatures/SignUp/Services/SignUpWebService.swift
@@ -20,7 +20,17 @@ class SignUpWebService{
     
     func signUp(withForm formModel: SignUpFormRequestModel, completion: @escaping (SignUpResponseModel?, SignUpErrors?)->()){
         
+        guard let url = URL(string: urlString) else{
+            // TODO create a unit test
+            return
+        }
+        //POST
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept") //accept json doc
         
+        request.httpBody = try? JSONEncoder().encode(formModel)
         
     }
 
