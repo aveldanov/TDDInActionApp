c4b544bea041ddbd8ffcfa87764ccb5bb132a454
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-20 19:34:14 -0700

    iOS

---
+++ TDDPhtotoAppFeatures/TDDPhtotoAppFeaturesTests/SignUpFormModelValidatorTests.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpFormModelValidatorTests.swift
+//  TDDPhtotoAppFeaturesTests
+//
+//  Created by Anton Veldanov on 6/20/21.
+//
+
+import XCTest
+
+class SignUpFormModelValidatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+
+}
