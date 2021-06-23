4adede0de4353fd148ab95dec3881992f5ca3b03
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-22 18:27:22 -0700

    iOS

--- TDDPhtotoAppFeatures/TDDPhtotoAppFeaturesTests/SignUp/MockURLProtocol.swift
+++ TDDPhtotoAppFeatures/TDDPhtotoAppFeaturesTests/SignUp/MockURLProtocol.swift
@@ -9,5 +9,19 @@ import Foundation
 
 class MockURLProtocol: URLProtocol{
     
+    static var stubResponseData: Data?
+    
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        client?.urlProtocol(self, didLoad: <#T##Data#>)
+    }
     
 }
