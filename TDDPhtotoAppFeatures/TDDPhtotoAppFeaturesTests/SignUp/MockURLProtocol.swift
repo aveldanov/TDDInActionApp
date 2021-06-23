ee648108be83ae50a7659a12c35c399824168df1
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-22 18:21:31 -0700

    iOS

---
+++ TDDPhtotoAppFeatures/TDDPhtotoAppFeaturesTests/SignUp/MockURLProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  MockURLProtocol.swift
+//  TDDPhtotoAppFeaturesTests
+//
+//  Created by Anton Veldanov on 6/22/21.
+//
+
+import Foundation
+
+class MockURLProtocol: URLProtocol{
+    
+    
+}
